ca49aa35479b217275b908a9b4a349cc57de35a3
DESKTOP-2AG9GJT\Alejandro Cruz <alejandroxdlx@gmail.com>
2023-03-28 10:43:04 +0100

    creado componente del footer y añadido firebase

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+    timetable = [
+        "12:00-22:00",
+        "12:00-22:00",
+        "12:00-22:00",
+        "12:00-22:00",
+        "12:00-22:00",
+        "12:00-22:00",
+        "12:00-22:00"
+    ]
+
+}
