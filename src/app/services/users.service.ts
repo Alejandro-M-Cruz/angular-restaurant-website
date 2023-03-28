01e52f4d6535a65ddb4cbc71abedcfc43a51531e
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-03-28 23:18:21 +0100

    actualizado servicio de usuario

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import {getFirestore} from "@angular/fire/firestore";
 import {getAuth} from "@angular/fire/auth";
 
 @Injectable({
@@ -7,7 +6,6 @@ import {getAuth} from "@angular/fire/auth";
 })
 export class UsersService {
   private auth = getAuth()
-  private firestore = getFirestore()
 
   getUser() {
     return this.auth.currentUser
