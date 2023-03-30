d5edd9f5c2826beba797205d074e990595fceec4
Alejandro-M-Cruz <alejandromatiascc@gmail.com>
2023-03-30 22:44:39 +0100

    componente user-reservations y reservations-table

--- src/app/pages/user-reservations/user-reservations.component.ts
+++ src/app/pages/user-reservations/user-reservations.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import {Reservation} from "../../model/reservation.model";
 
 @Component({
@@ -7,18 +7,12 @@ import {Reservation} from "../../model/reservation.model";
   styleUrls: ['./user-reservations.component.css']
 })
 export class UserReservationsComponent {
-  reservations = [
-    {date: new Date(), time: "12:30", customers: 5, userId: "useruid"},
-    {date: new Date(), time: "12:30", customers: 5, userId: "useruid"},
-    {date: new Date(), time: "12:30", customers: 5, userId: "useruid"},
-    {date: new Date(), time: "12:30", customers: 5, userId: "useruid"}
+  displayedColumns = ['datetime', 'customers'];
+  userReservations: Reservation[] = [
+    {datetime: new Date(), customers: 5, userId: '123'},
+    {datetime: new Date(), customers: 5, userId: '123'},
+    {datetime: new Date(), customers: 5, userId: '123'},
+    {datetime: new Date(), customers: 5, userId: '123'}
   ]
 
-  displayedColumns: string[] = ['date', 'time', 'customers', 'userId'];
-  clickedRows = new Set<Reservation>();
-
-  onRowClicked(row: Reservation) {
-    this.clickedRows.clear()
-    this.clickedRows.has(row) ? this.clickedRows.delete(row) : this.clickedRows.add(row)
-  }
 }
