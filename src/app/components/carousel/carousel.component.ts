22c33115f262a73cf18dcb72a8021d65cc1a2824
Cristhian López <holbey12@gmail.com>
2023-04-18 13:05:30 +0200

    carousel bootstrap added and removed the previous one

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent {
 
+export class CarouselComponent {
+  @Input() welcome: string;
 }
