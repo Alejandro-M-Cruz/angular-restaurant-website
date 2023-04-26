f160a964a4edd63b8ef27063395ee23a97c5c5a8
Cristhian López <holbey12@gmail.com>
2023-04-26 03:59:30 +0200

    stripe checkout session in progress

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
-import {TranslocoService} from '@ngneat/transloco';
+import { TranslocoService } from '@ngneat/transloco';
+import { StripeCheckoutService } from 'src/app/services/stripe-checkout.service';
+import { StripeStoreService } from 'src/app/services/stripe-store.service';
 
 @Component({
   selector: 'app-cart',
@@ -14,6 +16,8 @@ export class CartComponent {
   constructor(
     private cartService: CartService,
     private readonly translationService: TranslocoService,
+    private stripeCheckoutService: StripeCheckoutService,
+    private stripeStoreService: StripeStoreService
   ) { }
 
   getActiveLanguage() {
@@ -22,6 +26,26 @@ export class CartComponent {
 
   clearCart(){
     this.cartService.clearCart()
+  }
+
+  async buy(){
+    type line_items = {price: string, quantity: number}
+    let line_items: line_items[] = []
+    await Promise.all(
+      this.cartService.getCartItems().map(async (cartItem) => {
+        const product = await this.stripeStoreService.retrieveProductInformationById(cartItem.menuItem.idStripe!);
+        line_items.push({
+          price: product.default_price,
+          quantity: cartItem.amount,
+        });
+      })
+    );
+
+    return await this.stripeCheckoutService.createCheckoutSession(
+      line_items,
+      'https://example.com/success',
+      'https://example.com/cancel' 
+    )
   }
 
 }
