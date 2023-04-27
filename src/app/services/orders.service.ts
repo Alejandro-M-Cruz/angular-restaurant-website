5ba0e83b6dfe5decd319992f2194bce20e685510
DESKTOP-2AG9GJT\Alejandro Cruz <alejandroxdlx@gmail.com>
2023-04-27 13:16:33 +0100

    actualizado el servicio de pedidos para distinguir pedidos activos y terminados

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import {addDoc, collection, collectionData, Firestore, orderBy, query, where} from "@angular/fire/firestore";
 import {Order} from "../model/order.model";
-import {Observable} from "rxjs";
-import {Auth} from "@angular/fire/auth";
+import {map, Observable} from "rxjs";
 import {UserService} from "./user.service";
 
 @Injectable({
@@ -12,22 +11,38 @@ export class OrdersService {
   private readonly ordersCollection = collection(this.firestore, 'orders')
   constructor(private readonly firestore: Firestore, private readonly userService: UserService) { }
 
-  getOrders(): Observable<Order[]> {
+  getAllOrders(): Observable<Order[]> {
     const q = query(this.ordersCollection, orderBy('creationTimestamp'))
-    return collectionData(q, {idField: 'id'}) as Observable<Order[]>
+    return collectionData(q, {idField: 'id'})
+      .pipe(
+        map(orders => orders.map((order: any) => order.creationtimestamp = order.creationTimestamp.toDate()))
+      ) as Observable<Order[]>
   }
 
-  getUserOrders(): Observable<Order[]> {
+  getActiveOrders(): Observable<Order[]> {
+    const today = new Date()
+    today.setHours(0, 0, 0, 0)
+    const q = query(this.ordersCollection, where('creationTimestamp', '>=', today))
+    return collectionData(q, {idField: 'id'})
+      .pipe(
+        map(orders => orders.map((order: any) => order.creationtimestamp = order.creationTimestamp.toDate()))
+      ) as Observable<Order[]>
+  }
+
+  getAllUserOrders(): Observable<Order[]> {
     const q = query(
       this.ordersCollection,
       where('userId', '==', this.userService.getCurrentUser()!.uid),
       orderBy('creationTimestamp')
     )
-    return collectionData(q, {idField: 'id'}) as Observable<Order[]>
+    return collectionData(q, {idField: 'id'})
+      .pipe(
+        map(orders => orders.map((order: any) => order.creationtimestamp = order.creationTimestamp.toDate()))
+      ) as Observable<Order[]>
   }
 
   async addOrder(order: Order): Promise<void> {
-    await addDoc(this.ordersCollection, order)
+    await addDoc(this.ordersCollection, {...order, userId: this.userService.getCurrentUser()!.uid})
   }
 
 }
