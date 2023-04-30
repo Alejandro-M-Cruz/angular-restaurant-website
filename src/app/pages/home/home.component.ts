e7a1ed021c7f6a99c7ee52dc892964a6e3cf1868
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2023-04-30 17:19:00 +0100

    Arreglado home del admin

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from "../../services/user/user.service";
 import {User} from "../../model/user";
 import {first, Subscription} from "rxjs";
+import { PermissionsService } from 'src/app/services/user/permissions.service';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +10,18 @@ import {first, Subscription} from "rxjs";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  userInfo: User | null = null
-  userInfoSubscription?: Subscription
+  userInfo: User | null = null;
+  userInfoSubscription?: Subscription;
+  userIsAdmin:boolean;
 
-  constructor(private readonly userService: UserService) {}
+
+  constructor(private readonly userService: UserService, private permissionsService:PermissionsService) {}
 
   ngOnInit() {
     this.userInfoSubscription = this.userService.getCurrentUserObservable().subscribe(userInfo => {
       this.userInfo = userInfo
     })
+    this.permissionsService.isAdmin().subscribe(isAdmin => this.userIsAdmin = isAdmin);
   }
 
   ngOnDestroy() {
