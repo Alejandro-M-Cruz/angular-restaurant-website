bbb11865b2e2e5196c267c6a9b16fce8320adb1e
NataliaGlezz <natglezgil@gmail.com>
2023-05-15 13:32:17 +0100

    modelos emails corregidos

--- src/app/model/email.model.ts
+++ src/app/model/email.model.ts
@@ -35,37 +35,53 @@ export class buildEmail{
 
   private  welcomeEmailBody(userName:string){
     const subject = "Registro exitoso"
-    const body =`<p>¡Bienvenido/a a nuestra familia,${userName}</p>`
+    const html_body =`<p>¡Bienvenido/a a nuestra familia,${userName}</p>`
   +            "<p>Esperamos que tengas una experiencia agradable con nosotros</p>"
   +            "<p>Ahora puedes realizar reservas y pedidos desde nuestra web</p>"
   +            "<p>¡Ahora puedes darnos tu opinión y ayudarnos a mejorar!</p>"
   +            "<p>Esperamos tu visita</p>"
-    return {subject:subject, body:body};
+    const body =`¡Bienvenido/a a nuestra familia,${userName}`
+  +            "Esperamos que tengas una experiencia agradable con nosotros"
+  +            "Ahora puedes realizar reservas y pedidos desde nuestra web"
+  +            "¡Ahora puedes darnos tu opinión y ayudarnos a mejorar!"
+  +            "Esperamos tu visita"
+    return {subject:subject, html_body:html_body, body:body};
   }
 
   private  orderEmailBody(creationTimestamp:Date, city:string, country:string, line1:string, line2:string, postalCode:string, state:string){
     const subject = "Pedido confirmado";
-    const body = "<p>Información del pedido: </p>"
+    const html_body = "<p>Información del pedido: </p>"
   +              "<ul>"
   +               `<li>Día: ${creationTimestamp.getDay} </li>`
   +               `<li>Realizado a las: ${creationTimestamp.getHours} </li>`
   +               `<li>Dirección de envío: ${city} ${country} ${line1} ${line2} ${postalCode} ${state} </li>`
   +              "</ul>"
   +              "<p>¡Gracias por pedir en La Nostra Casa!</p>"
-    return {subject:subject, body:body};
+    const body = "Información del pedido:"
+  +               `Día: ${creationTimestamp.getDay}`
+  +               `Realizado a las: ${creationTimestamp.getHours}`
+  +               `Dirección de envío: ${city} ${country} ${line1} ${line2} ${postalCode} ${state}`
+  +               "¡Gracias por pedir en La Nostra Casa!"
+    return {subject:subject, html_body:html_body, body:body};
   }
 
   private  reservationEmailBody(date:number, time:string, customers:number){
     const subject = "Reserva confirmada"
-    const body = "<p>¡Gracias por reservar con La Nostra Casa!</p>"
+    const html_body = "<p>¡Gracias por reservar con La Nostra Casa!</p>"
   +              "<p>Datos de la reserva:</p>"
   +              "<ul>"
   +               `<li>Fecha: ${date} </li>`
   +               `<li>Hora: ${time} </li>`
   +               `<li>Número de personas: ${customers} </li>`
   +              "</ul>"
-  +            "<p><p>Guarde este mail en caso de cualquier problema con la reserva.</p></p>"
-    return {subject:subject, body:body};
+  +              "<p>Guarde este mail en caso de cualquier problema con la reserva.</p>"
+    const body = "¡Gracias por reservar con La Nostra Casa!"
+  +              "Datos de la reserva:"
+  +               `Fecha: ${date}`
+  +               `Hora: ${time}`
+  +               `Número de personas: ${customers}`
+  +              "Guarde este mail en caso de cualquier problema con la reserva."
+    return {subject:subject, html_body:html_body, body:body};
   }
 
 }
