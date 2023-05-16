a13a3fc6c68e40fc09509fe7c27b5217bd27eb8f
DESKTOP-2AG9GJT\Alejandro Cruz <alejandroxdlx@gmail.com>
2023-05-16 12:23:41 +0100

    creado selector de envío a domicilio y formulario de dirección en proceso

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -3,6 +3,8 @@ import { CartService } from 'src/app/services/orders/cart.service';
 import { TranslocoService } from '@ngneat/transloco';
 import {OrderCheckoutService} from "../../services/orders/order-checkout.service";
 import {Location} from "@angular/common";
+import {Address} from 'src/app/model/order.model';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,12 +12,33 @@ import {Location} from "@angular/common";
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-  cartItems = this.cartService.getCartItems();
+  cartItems = this.cartService.getCartItems()
+  addressForm = this.formBuilder.group({
+    city: ['', [Validators.maxLength(Address.CITY_MAX_LENGTH), Validators.required]],
+    street: ['', [Validators.maxLength(Address.STREET_MAX_LENGTH), Validators.required]],
+    streetNumber: ['', [
+      Validators.min(0),
+      Validators.max(Address.MAX_STREET_NUMBER),
+      Validators.required
+    ]],
+    postalCode: ['', [
+      Validators.min(Address.MIN_POSTAL_CODE),
+      Validators.max(Address.MAX_POSTAL_CODE),
+      Validators.required
+    ]],
+    additionalData: this.formBuilder.group({
+      storey: ['', Validators.min(0), Validators.max(100)],
+      doorNumber: ['', Validators.min(0), Validators.max(10000)],
+      comments: ['', Validators.maxLength(100)]
+    })
+  })
+  isHomeDelivery = false
 
   constructor(
     private readonly cartService: CartService,
     private readonly orderCheckoutService: OrderCheckoutService,
     private readonly translationService: TranslocoService,
+    private readonly formBuilder: FormBuilder,
     public readonly location: Location
   ) { }
 
@@ -28,7 +51,14 @@ export class CartComponent {
   }
 
   async goToCheckout(){
-    this.orderCheckoutService.goToCheckout()
+    this.orderCheckoutService.goToCheckout(
+      this.isHomeDelivery ?
+        this.addressForm.value as Address :
+        undefined
+    )
   }
 
+  onDeliveryOptionChanged(isHomeDelivery: boolean) {
+    this.isHomeDelivery = isHomeDelivery
+  }
 }
--- src/app/model/order.model.ts
+++ src/app/model/order.model.ts
@@ -1,6 +1,11 @@
 import {CartItem} from "./cart-item.model";
 
-export interface Address {
+export class Address {
+  static readonly CITY_MAX_LENGTH = 100
+  static readonly STREET_MAX_LENGTH = 100
+  static readonly MIN_POSTAL_CODE = 35000
+  static readonly MAX_POSTAL_CODE = 35999
+  static readonly MAX_STREET_NUMBER = 1000
   city: string
   country: string
   line1: string
@@ -15,17 +20,17 @@ interface Address {
   street: string
   streetNumber: number
   postalCode: string
-  additionalData?: {
-    storey: number
-    doorNumber: number
-    comments: string
+  additionalData: {
+    storey: number | null
+    doorNumber: number | null
+    comments: string | null
   }
 }
 */
 
 export class Order {
   static readonly MAX_TOTAL_ITEMS = 30
-  static readonly HOME_DELIVERY_FEE = 2.99
+  static readonly HOME_DELIVERY_FEE = 3.99
   static readonly TIP_OPTIONS = [0, 1, 2, 5]
   static readonly MAX_TIP = 100
   id?: string;
